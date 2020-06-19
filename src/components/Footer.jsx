73dfe651936f7790dadf96a5a0d4aadff0ff7646
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-19 07:09:07 -0400

    Footer de la app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer py-3 border-top text-center">
+      Gallery Theme &copy;{new Date().getFullYear()}
+    </div>
+  );
+}
+
+export default Footer;
