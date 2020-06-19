1204c91fdb27bb43f006d213821afb14e3cab974
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-19 07:13:11 -0400

    Items del menú de categorías

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+const CategoryItem = (props) => {
+  const setCurrentCategory = (cat) => {
+    props.selectedCategory(cat)
+  }
+
+  return (
+    <NavLink exact to={`/category/${props.category.id}`}>
+      <li
+        className="list-group-item border-bottom category-item"
+        onClick={() => setCurrentCategory(props.category.name)}
+      >
+        {props.category.name}
+      </li>    
+    </NavLink>
+  );
+}
+
+export default CategoryItem;
