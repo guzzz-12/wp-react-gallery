2314a91d5a6d1c5901d36f03e1d2b3b3cb6d12bc
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-19 11:13:50 -0400

    Mostrar skeleton mientras carga el menú de categorías

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {NavLink} from "react-router-dom";
 import axios from "axios";
+import Skeleton, {SkeletonTheme} from "react-loading-skeleton";
 import CategoryItem from "./CategoryItem";
 
 const CategoryMenu = (props) => {
@@ -23,20 +24,33 @@ const CategoryMenu = (props) => {
 
   return (
     <ul className="list-group">
-      <NavLink exact to={`/`}>
-        <li className="list-group-item border-bottom category-item">
-          All
-        </li>
-      </NavLink>
-      {categories.map(category => {
-        return (
-          <CategoryItem
-            key={category.id}
-            category={category}
-            selectedCategory={props.selectedCategory}
-          />
-        )
-      })}
+      <React.Fragment>
+        {isLoading &&
+          <SkeletonTheme color="#a0a0a0" highlightColor="#c5c5c5">
+            <p>
+              <Skeleton count={7} height={35}/>
+            </p>
+          </SkeletonTheme>
+        }
+        {!isLoading &&
+          <React.Fragment>
+            <NavLink exact to={`/`}>
+              <li className="list-group-item border-bottom category-item">
+                All
+              </li>
+            </NavLink>
+            {categories.map(category => {
+              return (
+                <CategoryItem
+                  key={category.id}
+                  category={category}
+                  selectedCategory={props.selectedCategory}
+                />
+              )
+            })}
+          </React.Fragment>        
+        }
+      </React.Fragment>
     </ul>
   );
 }
