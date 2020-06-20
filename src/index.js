c37d1eb758f96a4ed3552a10a90a922f5b461a54
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-20 13:44:40 -0400

    Ajustes menores

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import App from "./App";
+axios.defaults.baseURL = "http://localhost/gallery-theme/wp-json/wp/v2"
 
 ReactDOM.render(
   <React.StrictMode>
